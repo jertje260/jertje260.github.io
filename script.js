c1a12f5c61db8167fe88af9b11f1add1cb8a4156
jertje260 <jwbroekh@avans.nl>
2016-08-29 16:09:01 +0200

    bugfix

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ var idsURL = "pots.json";
 var allItems;
 var rsbase = "http://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item="
 var baseurl = "https://api.rsbuddy.com/grandExchange?a=guidePrice&i=";
+var pngurl = "http://cdn.rsbuddy.com/items/"; //+id.png 
 var items = [];
 var loading = false;
+var clickedItems;
 
 var loadItem = function (itemid, itemlistId, three) {
     //item.load(baseurl+itemid);
@@ -80,20 +82,22 @@ var checkDone = function () {
 
 var loadTable = function () {
     if (!loading) {
+        
         loading = true;
         console.log("loading table");
         for (i = 0; i < items.length; i++) {
             if (items[i].three.buying != 0 && items[i].four.selling != 0) {
                 var newRow = "<tr><td>" + items[i].three.name.replace("(3)", "") +
                     "</td><td>" + items[i].three.buying +
-                    "</td><td>" + items[i].four.buying +
+                    "</td><td>" + items[i].four.selling +
                     "</td><td>" + (parseInt(items[i].three.buying) / 3) +
                     "</td><td>" + (parseInt(items[i].four.selling) / 4) +
                     "</td><td>" + ((parseInt(items[i].four.selling) / 4) - (parseInt(items[i].three.buying) / 3)) +
                     "</td></tr>"
                 $('#decanting tbody').append(newRow);
+                
             }else {
-                console.log(items[i]);
+                //console.log(items[i]);
             }
         }
         $('#decanting').DataTable({
@@ -122,4 +126,4 @@ $('#3').change(function(){
 });
 $('#4').change(function(){
     recalculate();
-})
+});
